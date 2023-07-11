ad5daf2bae28b31f137f5439b3e78129b3c80270
Nicolas Mateo Uroz <nicolasmateo1995@gmail.com>
2023-07-11 14:56:02 +0200

    second commit with bug

--- src/component/Todo.jsx
+++ src/component/Todo.jsx
@@ -1,15 +1,47 @@
-export const Todo =() =>{
+import React, { useState } from "react";
+export const Todo = () => {
+    const initialTodos = ["My first todo", "My second todo"];
+    const [todos, setTodos] = useState(initialTodos);
+    const [checkedTodos, setCheckedTodos] = useState(
+        new Array(initialTodos.length).fill(false)
+    )
+
+    const handleCheckboxChange = (index) => {
+        const newCheckedTodos = [...checkedTodos]
+        newCheckedTodos[index] = !newCheckedTodos[index];
+        setCheckedTodos(newCheckedTodos);
+    }
     return (
         <>
         <ul>
-            <li>
-                <input type="checkbox" /> My first todo
-            </li>
-            <li>
-                <input type="checkbox" /> My second todo
-            </li>
+            {todos.map((todo, index) => (
+                <li key={index}>
+                    <input 
+                    type="checkbox"
+                    checked={checkedTodos[index]}
+                    onChange={()=> handleCheckboxChange(index)} 
+                    /> {" "}
+                    {todo}
+                </li>
+            ))}
         </ul>
         </>
     )
-}
+} 
 
+
+/**import React, { useState } from "react";
+
+export default function TodoList() {
+  const initialTodos = ["My first todo", "My second todo"];
+  const [todos, setTodos] = useState(initialTodos);
+  return (
+    <ul>
+      {todos.map((todo, index) => (
+        <li key={index}>
+          <input type="checkbox" /> {todo}
+        </li>
+      ))}
+    </ul>
+  );
+}**/
