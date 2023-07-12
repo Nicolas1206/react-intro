1f357c0082e3fea0b6a41ba436619aec465eccde
Nicolas Mateo Uroz <nicolasmateo1995@gmail.com>
2023-07-12 16:05:43 +0200

    chore:state

--- src/component/Todo.jsx
+++ src/component/Todo.jsx
@@ -1,47 +1,35 @@
-import React, { useState } from "react";
+import { useState } from "react"; 
+
 export const Todo = () => {
-    const initialTodos = ["My first todo", "My second todo"];
-    const [todos, setTodos] = useState(initialTodos);
-    const [checkedTodos, setCheckedTodos] = useState(
-        new Array(initialTodos.length).fill(false)
-    )
-
-    const handleCheckboxChange = (index) => {
-        const newCheckedTodos = [...checkedTodos]
-        newCheckedTodos[index] = !newCheckedTodos[index];
-        setCheckedTodos(newCheckedTodos);
-    }
-    return (
-        <>
-        <ul>
-            {todos.map((todo, index) => (
-                <li key={index}>
-                    <input 
-                    type="checkbox"
-                    checked={checkedTodos[index]}
-                    onChange={()=> handleCheckboxChange(index)} 
-                    /> {" "}
-                    {todo}
-                </li>
-            ))}
-        </ul>
-        </>
-    )
-} 
-
-
-/**import React, { useState } from "react";
-
-export default function TodoList() {
-  const initialTodos = ["My first todo", "My second todo"];
+    // J'initialise un tableau d'objet 
+  const initialTodos = [{todoName : "My first todo",todoStatut: false}, {todoName : "My second todo",todoStatut: false} ];
+    // j'initialise mon tableau dans mon useState
   const [todos, setTodos] = useState(initialTodos);
+    // fonction pour prendre en main le changement d'index des TODO
+  const handleTodoChange = (index) => {
+    // Copie en mémoire(DOM) de notre tableau contenant nos todos
+    const updatedTodos = [...todos];
+    // condition pour savoir si l'index de todoStatut est coché ou non 
+    updatedTodos[index].todoStatut = !updatedTodos[index].todoStatut
+    // Appliquer la variable updateedTodos dans notre useState setTodos
+    // Afin de reconstruire notre stateTodo et le rafraichir dynamiquement
+    setTodos(updatedTodos)
+    console.log(updatedTodos[index].todoStatut)
+    console.log(todos)
+    
+  }
   return (
     <ul>
-      {todos.map((todo, index) => (
+      {todos.map((todo,index) => (
         <li key={index}>
-          <input type="checkbox" /> {todo}
-        </li>
+        <input
+          type="checkbox"
+          checked={todo.todoStatut}
+          onChange={()=>handleTodoChange(index)}
+        />
+        {todo.todoName}
+      </li>
       ))}
     </ul>
   );
-}**/
+}
